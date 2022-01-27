430a44d2515ab9a1f5ee272c316aee2ecc6c1647
Charlie Highum <chighum41@gmail.com>
2022-01-27 01:34:21 -0700

    fully functional projects page with screenshots of each app that pop up a modal with name, description, and links to deployed and github repo

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -4,52 +4,72 @@ import projects from "../data/ProjectData";
 // this will generate each individual project
 function Project() {
   return (
-    <div>
-      {projects.map((project) => {
-        return (
-          <div>
-            <button
-              type="button"
-              class="btn btn-primary"
-              data-bs-toggle="modal"
-              data-bs-target={project.id}
-            >
-              <img src={project.image}></img>
-            </button>
+    <div className="col-9">
+      <div className="row">
+        {projects.map((project) => {
+          return (
+            <div className="col-6">
+              <button
+                type="button"
+                className="btn"
+                data-bs-toggle="modal"
+                data-bs-target={project.id}
+              >
+                <img
+                  src={project.image}
+                  style={{ width: 500, height: 300 }}
+                ></img>
+              </button>
 
-            <div
-              class="modal fade"
-              id={project.name}
-              tabindex="-1"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
-            >
-              <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">
-                      {project.name}
-                    </h5>
-                    <button
-                      type="button"
-                      class="btn-close"
-                      data-bs-dismiss="modal"
-                      aria-label="Close"
-                    ></button>
-                  </div>
-                  <div class="modal-body">
-                    <p>{project.description}</p>
-                  </div>
-                  <div class="modal-footer">
-                    <a href={project.deployed}>Deployed Application</a>
-                    <a href={project.github}>Github Repository</a>
+              <div
+                className="modal fade"
+                id={project.name}
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog modal-dialog-centered">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5
+                        className="modal-title text-center"
+                        id="exampleModalLabel"
+                      >
+                        {project.name}
+                      </h5>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body">
+                      <p>{project.description}</p>
+                    </div>
+                    <div className="modal-footer">
+                      <a
+                        href={project.deployed}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Deployed Application
+                      </a>
+                      <a
+                        href={project.github}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Github Repository
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -6,8 +6,15 @@ function Projects() {
     document.title = "Projects";
   });
   return (
-    <div>
-      <Project />
+    <div className="container">
+      <div className="row">
+        <p className="col-3 text-center">
+          To the right you can find screenshots of some of my favorite and
+          proudest projects that I've done in my short time as a full stack web
+          developer.
+        </p>
+        <Project />
+      </div>
     </div>
   );
 }
