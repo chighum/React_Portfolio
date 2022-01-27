69238a1da174e74e25a77219348942728336712f
Charlie Highum <chighum41@gmail.com>
2022-01-26 22:32:34 -0700

    added images folder with images to be used in the page. also added data folder with projectdata.js, an array of objects including all the necessary info on my projects

---
+++ client/src/data/ProjectData.js
@@ -0,0 +1,52 @@
+const projects = [
+  {
+    name: "HappyCamper",
+    description:
+      "HappyCamper is a group project for the DU Coding Bootcamp that allows the user to find national park campsites in the state of Colorado or outdoor adventure books if the weather is bad.",
+    languages: "Node.js, MySQL",
+    image: "",
+    github: "https://github.com/afarr002/TrailTrack",
+    deployed: "https://floating-shelf-98484.herokuapp.com/",
+  },
+  {
+    name: "FeedMe",
+    description:
+      "FeedMe is a group project for the DU Coding Bootcamp that allows the user to input ingredients they have in their kitchen and returns either food or drink recipes",
+    languages: "JavaScript, API",
+    image: "",
+    github: "https://github.com/KerryEames-Vris/ClassProject1-FeedMe",
+    deployed: "kerryeames-vris.github.io/ClassProject1-FeedMe/index.html",
+  },
+  {
+    name: "",
+    description: "",
+    languages: "",
+    image: "",
+    github: "",
+    deployed: "",
+  },
+  {
+    name: "",
+    description: "",
+    languages: "",
+    image: "",
+    github: "",
+    deployed: "",
+  },
+  {
+    name: "",
+    description: "",
+    languages: "",
+    image: "",
+    github: "",
+    deployed: "",
+  },
+  {
+    name: "",
+    description: "",
+    languages: "",
+    image: "",
+    github: "",
+    deployed: "",
+  },
+];
