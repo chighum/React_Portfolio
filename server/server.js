0862a0f06f1d4bfd80905e6986528815bec0f245
Charlie Highum <chighum41@gmail.com>
2022-03-03 11:04:17 -0700

    adding routes to server/server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,22 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const multiparty = require("multiparty");
 require("dotenv").config();
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+// app.use(routes);
+
+// Nodemailer
 const client = nodemailer.createTransport({
   service: "SendGrid",
   auth: {
