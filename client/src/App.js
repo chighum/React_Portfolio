dad1074e000c21ca8ebebba8c66b698e626d6117
Charlie Highum <chighum41@gmail.com>
2022-03-03 00:21:16 -0700

    fixed typo in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,42 +18,14 @@ function App() {
         <Header />
         <main className="my-5 py-5 min-vh-100">
           <Routes>
-            <Route exact path="/" element={<Home />}>
-            </Route>
-            <Route exact path="/background" element={<Background />}>
-            </Route>
-            <Route exact path="/coding" element={<Coding />}>
-            </Route>
-            <Route exact path="/hobbies" element={<Hobbies />}>
-            </Route>
-            <Route exact path="/projects" element={<Projects />}>
-            </Route>
-            <Route exact path="/contact" element={<Contact />}>
-            </Route>
-            <Route exact path="/resume" element={<Resume />}>
-            </Route>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/background" element={<Background />}></Route>
+            <Route exact path="/coding" element={<Coding />}></Route>
+            <Route exact path="/hobbies" element={<Hobbies />}></Route>
+            <Route exact path="/projects" element={<Projects />}></Route>
+            <Route exact path="/contact" element={<Contact />}></Route>
+            <Route exact path="/resume" element={<Resume />}></Route>
           </Routes>
-          <Route exact path="/" element={}>
-            <Home />
-          </Route>
-          <Route exact path="/background">
-            <Background />
-          </Route>
-          <Route exact path="/coding">
-            <Coding />
-          </Route>
-          <Route exact path="/hobbies">
-            <Hobbies />
-          </Route>
-          <Route exact path="/projects">
-            <Projects />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-          <Route exact path="/resume">
-            <Resume />
-          </Route>
         </main>
         <Footer />
       </div>
