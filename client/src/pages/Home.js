dc4c7042c8f6b3d03bff3e07c325db80fa89afe6
Charlie Highum <chighum41@gmail.com>
2022-03-15 23:00:42 -0600

    adding content for home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,61 +8,12 @@ function Home() {
 
   return (
     <div>
-      <div className="modal-dialog modal-fullscreen">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="landingModalLabel">
-              Welcome to my portfolio page!
-            </h5>
-          </div>
-          <div className="modal-body">
-            <img src="./images/logo.png" alt="Dark Logo"></img>
-          </div>
-          <div className="modal-footer">
-            <nav className="navbar me-2">
-              <div className="dropup">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <span>About Me</span>
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="/background">
-                      Background
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/coding">
-                      Web Development
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/hobbies">
-                      Hobbies
-                    </a>
-                  </li>
-                </ul>
-              </div>
-
-              <a className="nav-link" href="/projects">
-                <span>Projects</span>
-              </a>
-              <a className="nav-link" href="/contact">
-                <span>Contact Me</span>
-              </a>
-              <a className="nav-link" href="/resume">
-                <span>Resume</span>
-              </a>
-            </nav>
-          </div>
-        </div>
-      </div>
+      <h3>Welcome to my portfolio page!</h3>
+      <h5>
+        My name is Charlie Highum, and I am a Full-Stack Web Developer in
+        Denver, Colorado. I love to solve problems and learn new coding
+        frameworks or entire languages.
+      </h5>
     </div>
   );
 }
