cb1fd54e6383cea5f322f840a5c4211514ba8d15
Charlie Highum <chighum41@gmail.com>
2022-03-15 23:02:32 -0600

    more home page content

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,7 +12,9 @@ function Home() {
       <h5>
         My name is Charlie Highum, and I am a Full-Stack Web Developer in
         Denver, Colorado. I love to solve problems and learn new coding
-        frameworks or entire languages.
+        frameworks or entire languages. Please feel free to learn more about me
+        using the navigation bar at the top of the page, and thanks for stopping
+        by!
       </h5>
     </div>
   );
