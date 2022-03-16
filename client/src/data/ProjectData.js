6d8e310844adece68a07a632472f5b1487b1b137
Charlie Highum <chighum41@gmail.com>
2022-03-15 21:55:58 -0600

    adding new project to projects page

--- client/src/data/ProjectData.js
+++ client/src/data/ProjectData.js
@@ -1,9 +1,19 @@
 const projects = [
   {
-    name: "HappyCamper",
+    name: "Raining Cats and Dogs",
+    id: "#RainingCatsAndDogs",
+    description:
+      "This is an e-commerce site that allows users to create an account, update their user information, view pictures of dogs and cats for sale, add and delete items from a cart, and finally purchase said items. It was created using the MERN Stack with GraphQL and Stripe integration in order to process payments.",
+    languages: "Node.js, Mongoose, GraphQl, Stripe",
+    image: "./images/RainingCatsAndDogs.png",
+    github: "https://github.com/ccroberts1/project3",
+    deployed: "https://raining-cats-and-dogs.herokuapp.com/",
+  },
+  {
+    name: "Happy Camper",
     id: "#HappyCamper",
     description:
-      "HappyCamper is a group project for the DU Coding Bootcamp that allows the user to find national park campsites in the state of Colorado or outdoor adventure books if the weather is bad.",
+      "Created using Handlebars.js, MySQL, and Bulma Framework, this application will find all National Park Campgrounds in Colorado and display information for them. You've decided the weather is too bad in the area to go camping? Check out a list of our favorite outdoor adventure books, and see if something catches your eye!",
     languages: "Node.js, MySQL",
     image: "./images/HappyCamper.png",
     github: "https://github.com/afarr002/TrailTrack",
@@ -13,7 +23,7 @@ const projects = [
     name: "FeedMe",
     id: "#FeedMe",
     description:
-      "FeedMe is a group project for the DU Coding Bootcamp that allows the user to input ingredients they have in their kitchen and returns either food or drink recipes",
+      "FeedMe is a web application that takes user input of existing ingredients in their home and points them to possible recipes by utilizing the Spoonacular API, thus allowing the user to limit food waste and unnecessary trips to the grocery store.",
     languages: "JavaScript, API",
     image: "./images/FeedMe.png",
     github: "https://github.com/KerryEames-Vris/ClassProject1-FeedMe",
@@ -21,7 +31,7 @@ const projects = [
       "https://kerryeames-vris.github.io/ClassProject1-FeedMe/index.html",
   },
   {
-    name: "TextEditor",
+    name: "Text Editor",
     id: "#TextEditor",
     description:
       "This was a homework assignment for the DU Coding Bootcamp that required me to take an existing text editor app and add functionality for it to work as a PWA and function offline. This was done by implementing methods for storing and getting data from an IndexedDB database using the idb package, registering a service worker, and creating a manifest.json file.",
@@ -29,16 +39,6 @@ const projects = [
     image: "./images/JATE.png",
     github: "https://github.com/chighum/Homework19-TextEditor",
     deployed: "https://text-editor-cjh.herokuapp.com/",
-  },
-  {
-    name: "WeatherDashboard",
-    id: "#WeatherDashboard",
-    description:
-      "This was a homework assignment that required me to make a weather app using JavaScript and the OpenWeather API.",
-    languages: "JavaScript, OpenWeather API",
-    image: "./images/WeatherDashboard.png",
-    github: "https://github.com/chighum/Homework6-Weather_Dashboard",
-    deployed: "https://chighum.github.io/Homework6-Weather_Dashboard",
   },
 ];
 
