bae421fc6ac274b326a0fc80823f1d52c05caa00
Charlie Highum <chighum41@gmail.com>
2022-03-15 22:19:25 -0600

    editing content for about me pages

--- client/src/pages/About/Background.js
+++ client/src/pages/About/Background.js
@@ -11,15 +11,23 @@ function Background() {
         <div className="col-5 mx-5 align-self-center">
           <h3>Background</h3>
           <p>
-            I was born and raised in Denver, Colorado. I grew up with a total of
-            seven siblings so there was never a dull moment in our household. We
-            were all very competitive with one another growing up, pusing each
-            other to the very limit and sometimes over the edge, but that is how
-            I became the person that I am today. I have lived in Denver my
-            entire life aside from a four year stint in Austin, Texas. While in
-            Austin I enjoyed lots of live music and good food, and also managed
-            to obtain a B.S. in Petroleum Engineering from the University of
-            Texas along the way.
+            I was born and raised in Denver, Colorado where I lived my entire
+            life aside until moving to Austin, Texas to study at the University
+            of Texas. While in Austin I enjoyed lots of live music and good
+            food, and also managed to obtain a B.S. in Petroleum Engineering
+            from the University of Texas along the way. After graduating, I
+            moved to Vail, Colorado in order to be a ski instructor for a few
+            years, and had the time of my life.
+          </p>
+          <br />
+          <p>
+            Growing up in Denver I have spent a large amount of my time outside.
+            Whether I'm playing sports such as lacrosse, soccer, or football, or
+            spending time in the mountains skiing, fishing, hiking, or camping,
+            I still cannot get enough of the outdoors. I also love to get out of
+            the state and travel the world when I can. Some of my favorite
+            places I've traveled to around the world include Thailand,
+            Patagonia, Tuscany, and New Zealand.
           </p>
         </div>
         <div className="col-5 mx-5">
