929e7f0682bbd49479d6d0316b7bd7abfb93bba3
Charlie Highum <chighum41@gmail.com>
2022-03-16 18:20:59 -0600

    more landing page styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,30 +9,32 @@ function Home() {
   return (
     <div>
       <h3>Welcome to my portfolio page!</h3>
-      <div className="col-5 mx-5 align-self-center">
-        <h5>
-          My name is Charlie Highum, and I am a Full-Stack Web Developer in
-          Denver, Colorado.
-        </h5>
-        <br />
-        <h5>
-          I love to solve problems and learn new coding frameworks or entire
-          languages.
-        </h5>
-        <br />
-        <h5>
-          Please feel free to learn more about me using the navigation bar at
-          the top of the page.
-        </h5>
-        <br />
-      </div>
-      <div className="col-5 mx-5">
-        <img
-          src="./images/IMG_1988.jpeg"
-          alt="Photo of Me"
-          style={{ width: 300, height: 400 }}
-          className="rounded-3 shadow"
-        ></img>
+      <div className="row">
+        <div className="col-5 mx-5 align-self-center">
+          <h5>
+            My name is Charlie Highum, and I am a Full-Stack Web Developer in
+            Denver, Colorado.
+          </h5>
+          <br />
+          <h5>
+            I love to solve problems and learn new coding frameworks or entire
+            languages.
+          </h5>
+          <br />
+          <h5>
+            Please feel free to learn more about me using the navigation bar at
+            the top of the page.
+          </h5>
+          <br />
+        </div>
+        <div className="col-5 mx-5">
+          <img
+            src="./images/IMG_1988.jpeg"
+            alt="Photo of Me"
+            style={{ width: 300, height: 400 }}
+            className="rounded-3 shadow"
+          ></img>
+        </div>
       </div>
       <h3>Thanks for stopping by!</h3>
     </div>
