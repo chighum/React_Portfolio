872def0948687cc208f2fad7622b621b523f8e9f
Charlie Highum <chighum41@gmail.com>
2022-04-08 16:23:01 -0600

    trying to make image on background page rotate through a list of images

--- client/src/pages/About/Background.js
+++ client/src/pages/About/Background.js
@@ -1,29 +1,43 @@
 import React from "react";
 
+let index = 0;
+let images = ["./images/BroncosGame.JPG", "./images/JimmyBuffet.JPG"];
+
+function rotateImage() {
+  index++;
+  if (index == images.length) {
+    index = 0;
+  }
+  document.getElementById("images").src = images[index];
+
+  setTimeout(rotate, 3 * 1000);
+}
+
 function Background() {
   React.useEffect(() => {
     document.title = "Background";
   });
   return (
     <div>
-      <div className="row mb-5">
+      <div className="row">
         <h1>Background</h1>
         <div className="col-3 mx-5 align-self-center">
           <p>
             I was born and raised in Denver, Colorado where I lived my entire
-            life aside from four years spent in Austin, Texas to study at the
-            University of Texas. While in Austin I enjoyed lots of live music
-            and good food, and also managed to obtain a B.S. in Petroleum
-            Engineering along the way. After graduating, I moved to Vail,
-            Colorado in order to be a ski instructor for a few years, and had
-            the time of my life.
+            life until moving to Austin, Texas to study at the University of
+            Texas. While in Austin I enjoyed lots of live music, good food, a
+            perpetually over-hyped football team, and also managed to obtain a
+            B.S. in Petroleum Engineering along the way. After graduating, I
+            moved to Vail, Colorado in order to be a ski instructor for a few
+            years, and had the time of my life.
           </p>
         </div>
         <div className="col-3 mx-5">
           <img
             src="./images/Technologies.png"
-            alt="Family Photo"
+            alt="Photos of me with friends and family"
             className="rounded-circle shadow img-fluid"
+            id="images"
           ></img>
         </div>
         <div className="col-3 mx-5">
