f1a37ba62aebb3ea9e50cebf515e7f52e00147fe
Charlie Highum <chighum41@gmail.com>
2022-04-11 13:03:32 -0600

    styling navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,11 +20,8 @@ function Header() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div
-          className="collapse navbar-collapse d-flex"
-          id="navbarSupportedContent"
-        >
-          <div className="navbar-nav">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="navbar-nav d-flex">
             <a
               className="nav-link dropdown-toggle"
               href="/"
