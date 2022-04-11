1903482644a18588ec12b287ce5e7b06257229c0
Charlie Highum <chighum41@gmail.com>
2022-04-11 13:00:34 -0600

    styling navbar with collapsing hamburger menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ import "../styles/Header.css";
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg fixed-top">
+    <nav className="navbar navbar-expand-md fixed-top">
       <div className="container-fluid">
         <a className="navbar-brand ms-3" href="/">
           <img src={logo} alt="Logo I made on favicon.io" id="lightLogo"></img>
@@ -20,43 +20,48 @@ function Header() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <a
-            className="nav-link dropdown-toggle"
-            href="/"
-            id="navbarDropdown"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <span>About Me</span>
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li>
-              <a className="dropdown-item" href="/background">
-                Background
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="/coding">
-                Web Development
-              </a>
-            </li>
-            {/* <li>
+        <div
+          className="collapse navbar-collapse d-flex"
+          id="navbarSupportedContent"
+        >
+          <div className="navbar-nav">
+            <a
+              className="nav-link dropdown-toggle"
+              href="/"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <span>About Me</span>
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <a className="dropdown-item" href="/background">
+                  Background
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="/coding">
+                  Web Development
+                </a>
+              </li>
+              {/* <li>
           <a className="dropdown-item" href="/hobbies">
             Photos
           </a>
         </li> */}
-          </ul>
-          <a className="nav-link" href="/projects">
-            <span>Projects</span>
-          </a>
-          <a className="nav-link" href="/contact">
-            <span>Contact Me</span>
-          </a>
-          <a className="nav-link" href="/resume">
-            <span>Resume</span>
-          </a>
+            </ul>
+            <a className="nav-link" href="/projects">
+              <span>Projects</span>
+            </a>
+            <a className="nav-link" href="/contact">
+              <span>Contact Me</span>
+            </a>
+            <a className="nav-link" href="/resume">
+              <span>Resume</span>
+            </a>
+          </div>
         </div>
       </div>
     </nav>
